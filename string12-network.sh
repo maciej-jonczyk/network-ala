332d7b494222e9df612ad3eb5797324203eee7c6
maciej-jonczyk <56517984+maciej-jonczyk@users.noreply.github.com>
2023-08-14 14:58:00 +0200

    new IDs selection

--- string12-network.sh
+++ string12-network.sh
@@ -1,32 +1,20 @@
 # Uniprot - NAM list
 # /media/mj/ANTIX-LIVE/anno_fun_v45 directory
-# Starting point: file Zm-B73-REFERENCE-NAM.UniProt.proteins.tab
-grep 'Zm00001eb' Zm-B73-REFERENCE-NAM.UniProt.proteins.tab | cut -f1,6 -d" " | sort -k1,1 -t"      " > uniprot2nam
-# There is multiple UniProt IDs for one NAM
-# Unique NAM - 10492, there is 39756 genes in NAM (list in file genes_ids_v5)
-#***************************************************************************************8
-
-# I've found better mapping file.
-# The idea is to merge mappings, and select best NAMv5 for each UniProt ID mapped to v4
+# Files needed
+# 1. NAM - UniProt mapping Zea_mays.Zm-B73-REFERENCE-NAM-5.0.57.uniprot.tsv.gz
+# 2. UniProt - v4 mapping from STRING12 4577.protein.aliases.v12.0.txt.gz
+# 3. List of differentially expressed NAMs
+# The idea is to select NAM IDs matching UniProt IDs from STRING12.
+# From this list DE NAMs will be selected and for multimapped the best UniProt will be selected
 https://ftp.ebi.ac.uk/ensemblgenomes/pub/release-57/plants/tsv/zea_mays/Zea_mays.Zm-B73-REFERENCE-NAM-5.0.57.uniprot.tsv.gz
-# sort by UniProt ID
-zcat Zea_mays.Zm-B73-REFERENCE-NAM-5.0.57.uniprot.tsv.gz | sort -k4,4 -t"  " > x
-# 32796 NAM and 134901 UniProt IDS mapped -> there are multimappers
 
-# UniProt - v4 list from STRING12
+# list of UniProts in STRING12
 # /media/mj/ANTIX-LIVE/siec-ala-string directory
-zgrep 'Zm00001d' 4577.protein.aliases.v12.0.txt.gz | cut -f1,2 -d"      " | cut -f2 -d"." | tr '_' '\t' | cut -f1,3 -d"       " | sort -k1,1 -t"      " > uniprot2v4
-# There is 38140 unique UniProt IDs and 38515 unique v4 IDs
+zcat 4577.protein.aliases.v12.0.txt.gz | cut -f1 | tail -n +2 | cut -f2 -d"." | sort -u > uniprot.list
+# 39389 unique
 
-# Joining
-join -1 4 -2 1 -t" " x ../siec-ala-string/uniprot2v4 > x2
-# retain only NAM gene IDs, UniProt and mapping data
+zcat Zea_mays.Zm-B73-REFERENCE-NAM-5.0.57.uniprot.tsv.gz | sort -k4,4 -t"  " >x
+join -1 1 -2 4 -t" " ../siec-ala-string/uniprot.list x > x2
+# Selecting could be done also with grep but it selects also splicing forms, ie. UniProt IDs with subscript -1 or -2
 
-
-# After discarding v4 IDs (they were only used for narrowing number of UniProt IDs) reamains 35414 rows
-cut -f1,2 -d"      " x3 | sort -u | wc -l
-# And 31858 unique and 1734 multimapping UniProts
-
-# Now two things are possible:
-# 1. select the best NAM for each UniProt based on alignment data
-# 2. select NAM differentially expressed ina a given dataset and if needed 1. for them
+# The next step - selecting NAMs DE in Alas dataset
