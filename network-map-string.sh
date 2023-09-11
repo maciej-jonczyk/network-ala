c668b61452894a2fe41685b6114a85b6791fcada
maciej-jonczyk <56517984+maciej-jonczyk@users.noreply.github.com>
2023-09-11 17:57:38 +0200

    cleaning network for Cytoscape

--- network-map-string.sh
+++ network-map-string.sh
@@ -24,3 +24,23 @@ fastaindex -f Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.fa -i Zm-B73-REFERENC
 fastafetch -f Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.fa -i Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.in -q prot-ids -F > longest-prot.fa
 
 # 4. Mapping in on-line STRING122 tool
+
+# how many proteins are in network?
+zcat STRG0A98KWY.protein.links.full.v12.0.txt.gz | cut -f1 -d" " | tail -n +2 | sort -u | wc -l
+31878
+
+# so 80% of protein-coding genes are included in the network
+# network constructed with NAM-UP mapping gave 33629 UPs so slightly higher percent
+
+# 5. Preparing network for Cytoscape and joining with expression values
+# deleting not needed strings
+zcat STRG0A98KWY.protein.links.full.v12.0.txt.gz | sed 's/STRG0A98KWY\.//g;s/_P00[0-9]//g' > x
+# extracting header
+zcat STRG0A98KWY.protein.links.full.v12.0.txt.gz | head -n1 > xnagl
+# concatenating
+cat xnagl x > net-full
+# cleaning directory
+rm xnagl x
+
+
+
